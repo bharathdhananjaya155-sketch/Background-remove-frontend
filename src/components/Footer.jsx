66765cd0d80dd778a7c620163414d00627113347
Bharath D <james@Bharaths-MacBook-Air.local>
2026-03-23 17:50:24 +0530

    Updated footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
-import {assets, FOOTER_CONSTANTS} from "../assets/assets.js";
+import { assets, FOOTER_CONSTANTS } from "../assets/assets.js";
 
 const Footer = () => {
     return (
         <footer className="flex item-center justify-between gap-4 px-4 lg:px-44 py-3">
-            <img src={assets.logo} alt="logo" width={32}/>
+            <img src={assets.logo} alt="logo" width={32} />
             <p className="flex-1 border-l border-gray-100 max-sm:hidden">
-                &copy; {new Date().getFullYear()} @engineertalkswithbushan | All rights reserved.
+                &copy; {new Date().getFullYear()}
             </p>
             <div className="flex gap-3">
                 {FOOTER_CONSTANTS.map((item, index) => (
                     <a href={item.url} key={index} target="_blank" rel="noopener noreferrer">
-                        <img src={item.logo} alt="logo" width={32}/>
+                        <img src={item.logo} alt="logo" width={32} />
                     </a>
                 ))}
             </div>
